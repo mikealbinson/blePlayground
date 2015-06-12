d7edf8ee3c679f5f65f363b847118b32c2bd32a2
michaelalbinson <michael_albinson@ticketfly.com>
2015-06-11 17:43:25 -0700

    Initial Commit

---
+++ Documents/TicketflyBleExperiment/TicketflyBleExperiment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicketflyBleExperiment
+//
+//  Created by michael on 6/11/15.
+//  Copyright (c) 2015 Ticketfly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Documents/TicketflyBleExperiment/TicketflyBleExperiment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicketflyBleExperiment
+//
+//  Created by michael on 6/11/15.
+//  Copyright (c) 2015 Ticketfly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
